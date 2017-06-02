a1ce4c797d4d752bfe0b64641f3eeeb33605bbdd
Michal Stefanczuk <mstefanczuk@bluesoft.net.pl>
2017-06-02 20:22:09 +0200

    Add notifications

--- src/main/java/pl/stefanprogramuje/blog/controllers/PostsController.java
+++ src/main/java/pl/stefanprogramuje/blog/controllers/PostsController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.stefanprogramuje.blog.models.Post;
+import pl.stefanprogramuje.blog.services.NotificationService;
 import pl.stefanprogramuje.blog.services.PostService;
 
 @Controller
@@ -14,9 +15,18 @@ public class PostsController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private NotificationService notifyService;
+
     @RequestMapping("/posts/view/{id}")
     public String view(@PathVariable("id") Long id, Model model) {
         Post post = postService.findById(id);
+
+        if(post == null) {
+            notifyService.addErrorMessage("Cannot find post #" + id);
+            return "redirect:/";
+        }
+
         model.addAttribute("post", post);
         return "posts/view";
     }
---
+++ src/main/java/pl/stefanprogramuje/blog/services/NotificationService.java
@@ -0,0 +1,7 @@
+package pl.stefanprogramuje.blog.services;
+
+public interface NotificationService {
+
+    void addInfoMessage(String msg);
+    void addErrorMessage(String msg);
+}
